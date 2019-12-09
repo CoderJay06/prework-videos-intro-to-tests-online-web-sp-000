71ad36ee2cefb4277128f8682ce34748a3146f60
Jason J Cruz <jaycru06@yahoo.com>
2019-12-09 05:09:58 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -20,31 +20,3 @@ end
 
 
 
-
-
-
-
-
-
-
-
-require_relative '../conversions.rb'
-
-describe "conversions" do 
-  describe "#ounces_to_grams" do 
-    it "given 0, returns 0.0"
-    grams = ounces_to_grams(0)
-    expect(grams).to eq(0.0) 
-  end 
-end 
-
-
-
-
-
-
-
-
-
-
-
