c29cda3e50dc081e498107eba350135c39bdcab4
Jason J Cruz <jaycru06@yahoo.com>
2019-12-09 04:50:05 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -1,30 +1,22 @@
-# require_relative './conversions.rb'
+require_relative './conversions.rb'
 
-# puts "Given 0, it should return 0.0"
-# answer = ounces_to_grams(0)
-# if answer == 0.0
-#   puts "Passed!"
-# else
-#   puts "Test failed: got #{answer} instead"
-# end
-
-# puts "Given 1, it should return 28.3495"
-# answer = ounces_to_grams(1)
-# if answer == 28.3495
-#   puts "Passed!"
-# else
-#   puts "Test failed: got #{answer} instead"
-# end
-
-require './conversions.rb'
-
-puts 'ounces to grams, when given 0, returns 0.0'
+puts "Given 0, it should return 0.0"
 answer = ounces_to_grams(0)
 if answer == 0.0
-  puts "Passed"
-else 
+  puts "Passed!"
+else
   puts "Test failed: got #{answer} instead"
-end 
+end
+
+puts "Given 1, it should return 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
+
+
 
 
 
